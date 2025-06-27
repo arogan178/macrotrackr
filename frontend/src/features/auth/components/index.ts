8bab7c92a35bbc2d2706e23f5d414dc850dd5bef
arogan178 <andrea.bugeja@hotmail.com>
2025-06-27 15:18:17 +0200

    feat(auth): refactor registration process with improved step handling and validation; add constants for registration steps and error messages

--- frontend/src/features/auth/components/index.ts
+++ frontend/src/features/auth/components/index.ts
@@ -2,9 +2,5 @@ export { default as AuthForm } from "./AuthForm";
 export { default as ButtonModeToggle } from "./ButtonModeToggle";
 export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
-export {
-  StepIndicator,
-  StepOne,
-  StepTwo,
-  StepThree,
-} from "./RegisterFormSteps";
+export { StepOne, StepTwo, StepThree } from "./RegisterFormSteps";
+export { StepIndicator } from "./StepIndicator";
