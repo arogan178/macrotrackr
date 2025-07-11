1fc1a596c1a305119aedc51298f969cd7be5d550
arogan178 <andrea.bugeja@hotmail.com>
2025-07-11 15:52:29 +0200

    rename

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./test";
+import App from "./app";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
