96da62fbac69cf4b180060b9a3cbe5590c23f260
Andrea Bugeja <andrea.bugeja@hotmail.com>
2026-02-07 10:35:54 +0100

    Move floating notification offset into CSS

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,5 @@
 import AppRouter from "./AppRouter";
 
-// Global CSS variable for floating notification top offset (default 16px)
-// If your project sets CSS variables elsewhere (e.g., global.css), move this there.
-if (typeof document !== "undefined") {
-  const root = document.documentElement;
-  if (!root.style.getPropertyValue("--floating-notification-top")) {
-    root.style.setProperty("--floating-notification-top", "16px");
-  }
-}
-
 export default function App() {
   return <AppRouter />;
 }
