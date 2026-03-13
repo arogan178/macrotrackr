7b40944b6955ebe6980d58399631339d1698813f
Andrea Bugeja <andrea.bugeja@hotmail.com>
2026-03-13 21:24:48 +0100

    deslop 6

--- frontend/src/features/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./components";
-export { default as ResetPasswordPage } from "./pages/ResetPasswordPage";
--- frontend/src/features/landing/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as BlogArticlePage } from "./pages/BlogArticlePage";
-export { default as BlogIndexPage } from "./pages/BlogIndexPage";
-export { default as LandingPage } from "./pages/LandingPage";
-export { default as PrivacyPolicyPage } from "./pages/PrivacyPolicyPage";
-export { default as TermsAndConditionsPage } from "./pages/TermsAndConditionsPage";
